9edb281d2a33335e0f8900aeed1ed41cc18c8b00
Jaime Rios <jaime.madrigal@amk-technologies.com>
2018-05-21 14:33:35 -0500

    First commit.

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports.config = {
+  'homePageUrl': 'http://localhost:3000/#/add',
+  'campaign':{
+    "id":1,
+    "name":"AT&T_FACUNDO_MOBUSI",
+    "startDate":1526930561282,
+    "endDate":1526930561282,
+    "campaignType":"CPA",
+    "payout":1,
+    "notificationRate":0.1,
+    "budget":10,
+    "budgetPeriodicity":"DAILY",
+    "active":true,
+    "relatedEntities":{
+      "serviceId":1,
+      "channelId":1,
+      "providerId":1,
+      "notifyProvider":false
+    },
+    "campaignOrigin":"INTERNAL"
+  }
+};
