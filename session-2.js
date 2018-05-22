95d85a9c41b487b292616d6a3573bf3c63e4e22c
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-22 18:23:51 -0500

    Added user query.

--- session-2.js
+++ session-2.js
@@ -1,23 +1,31 @@
 const config = require('./config');
 const puppeteer = require('puppeteer'); // High level API to interact with headless Chrome
 const signale = require('signale');
-
+const query = require('cli-interact');
 
 const userSearch = async () => {
   const browser = await puppeteer.launch({headless: false, devtools: true});
   // const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  let url;
 
   signale.success('Funciona chingón 👊');
 
 
-  await page.goto();
+  // await page.goto();
 
 
   // Quieres tú ir a Google?
+  const answer = await query.getYesNo('Quieres tú ir a google');
+  await  (answer) ?ﬂ url = 'https://google.com' : url = 'https://wikipedia.org';
 
+  signale.success('Respondiste ⁉️' + url);
 
   // ¿Qué tu quieres buscar?
+
+
+  signale.success('Cerrando el navegador 🚪');
+ browser.close();
 
 }
 
